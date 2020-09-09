1f87261dcb683c1b12681c12b9b1664bd1c25ed8
Luis9620 <lasotomorales@miners.utep.edu>
2020-09-09 12:39:43 -0400

    Improvement in the cd and ls section.

--- Shell.py
+++ Shell.py
@@ -1,24 +1,30 @@
 import os, sys
 
-sys.ps1 = "$"
-while(True):
-    currentPath = os.getcwd()   #Get the current Path
-    sys.ps1 = "\033[30m" + currentPath + "$: "
-    userInput = input(sys.ps1)
-    if(userInput == "exit()"):
-        sys.exit()
-    userInput = userInput.split(" ")
-    lenOfInput = len(userInput)
-    if userInput[0] == "cd":
-        try:
-            os.chdir(currentPath + "/" + userInput[1])  # implement a try-catch
-        except:
-            continue
-    if userInput[0] == "ls":
-        directories = os.listdir()  #Get directories and files inside directory
-        for directory in directories:
-            if "." in directory:    #color the items without an extension(directories) blue
-                print("\033[30m" + directory, end=" ")
+try:
+    while True:
+        currentPath = os.getcwd()   #Get the current Path
+        sys.ps1 = str(os.getpid()) + " " + "\033[30m" + currentPath + "$: " #Adding the process ID for testing purpouses
+        userInput = input(sys.ps1)
+
+        if(userInput == "exit()"):
+            sys.exit()
+
+        userInput = userInput.split(" ")
+        lenOfInput = len(userInput)
+        if userInput[0] == "cd":
+            if os.path.isdir(currentPath + "/" + userInput[1]):
+                os.chdir(currentPath + "/" + userInput[1])  # implement a try-catch
             else:
-                print('\033[94m' + directory, end=" ")
-        print("\n")
+                os.write(1, (userInput[1] + " is not a directory.\n").encode())
+
+        if userInput[0] == "ls":
+            directories = os.listdir()  #Get directories and files inside directory
+            for directory in directories:
+                if os.path.isdir(currentPath + "/" + directory):
+                    print('\033[94m' + directory, end=" ")
+                else:
+                    print("\033[30m" + directory, end=" ")
+            print("\n")
+
+except KeyboardInterrupt:
+    os.write(1,("\nProcess Finished with exit code 0").encode())
