50710a48ff30d05236a9dd49deb0bdcf6c9e7598
Luis9620 <lasotomorales@miners.utep.edu>
2020-09-14 13:52:01 -0400

    Added the output redirection method and change methods names

--- Shell.py
+++ Shell.py
@@ -1,11 +1,13 @@
 import os, sys, re
-def changeDirectory(command):
+
+
+def change_directory(command):
     currentPath = os.getcwd()
     command, selectedDirectory = re.split(" ", command)
     if os.path.isdir(currentPath + "/" + selectedDirectory):
         os.chdir(currentPath + "/" + selectedDirectory)
 
-def showDirectories():
+def show_directories():
     currentPath = os.getcwd()
     directories = os.listdir()
     for directory in directories:
@@ -15,13 +17,43 @@ def showDirectories():
             print("\033[30m" + directory, end=" ")
     print("\n")
 
-def executeCommand(command):
+
+def execute(command):
+    for directory in re.split(':', os.environ['PATH']):
+        program = "%s/%s" % (directory, command[0])
+        try:
+            os.execve(program, command, os.environ)
+        except FileNotFoundError:
+            pass
+
+
+def output_to_file(command):
+    command, filePath = [i.strip() for i in re.split('>', command)]
+    filePath = os.getcwd() + '/' + filePath
+    command = [i.strip() for i in re.split(' ', command)]
+    rc = os.fork()
+    if rc < 0:
+        os.write(2, ("Fork failed").encode())
+        sys.exit(1)
+    elif rc == 0:
+        os.close(1)
+        sys.stdout = open(filePath, 'w+')
+        fd = sys.stdout.fileno()
+        os.set_inheritable(fd, True)
+        os.dup(fd)
+        execute(command)
+
+
+def process_command(command):
     if "exit()" in command:
         sys.exit(0)
     elif "cd" in command:
-        changeDirectory(command)
+        change_directory(command)
     elif "ls" in command:
-        showDirectories()
+        show_directories()
+    elif ">" in command:
+        output_to_file(command)
+
 
 try:
     sys.ps1 = os.environ['PS1']
@@ -30,10 +62,11 @@ except KeyError:
 if sys.ps1 is None:
     sys.ps1 = '$ '
 try:
+
     while True:
         os.write(1, sys.ps1.encode())
         userInput = input()
-        executeCommand(userInput)
+        process_command(userInput)
 
 except KeyboardInterrupt:
-    os.write(1,("\nProcess Finished with exit code 0").encode())
+    os.write(1, ("\nProcess Finished with exit code 0").encode())
