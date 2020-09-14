550dfe85f6eac82982643141303f995f713e1f53
Luis9620 <lasotomorales@miners.utep.edu>
2020-09-14 12:18:49 -0400

    Added methods for cd and ls

--- Shell.py
+++ Shell.py
@@ -1,42 +1,39 @@
-import os, sys
+import os, sys, re
+def changeDirectory(command):
+    currentPath = os.getcwd()
+    command, selectedDirectory = re.split(" ", command)
+    if os.path.isdir(currentPath + "/" + selectedDirectory):
+        os.chdir(currentPath + "/" + selectedDirectory)
+
+def showDirectories():
+    currentPath = os.getcwd()
+    directories = os.listdir()
+    for directory in directories:
+        if os.path.isdir(currentPath + "/" + directory):
+            print('\033[94m' + directory, end=" ")
+        else:
+            print("\033[30m" + directory, end=" ")
+    print("\n")
+
+def executeCommand(command):
+    if "exit()" in command:
+        sys.exit(0)
+    elif "cd" in command:
+        changeDirectory(command)
+    elif "ls" in command:
+        showDirectories()
 
 try:
+    sys.ps1 = os.environ['PS1']
+except KeyError:
+    sys.ps1 = '$ '
+if sys.ps1 is None:
+    sys.ps1 = '$ '
+try:
     while True:
-        currentPath = os.getcwd()   #Get the current Path
-        sys.ps1 = str(os.getpid()) + " " + "\033[30m" + currentPath + "$: " #Adding the process ID for testing purpouses
-        userInput = input(sys.ps1)
-
-        if(userInput == "exit()"):
-            sys.exit()
-
-
-        userInput = userInput.split(" ")
-        lenOfInput = len(userInput)
-
-        if userInput[0] == "cd":
-            rc = os.fork()
-            if rc < 0:
-                os.write(2, ("fork failed, returning %d\n" % rc).encode())
-                sys.exit(1)
-            elif rc == 0:
-                print("\n")
-                if os.path.isdir(currentPath + "/" + userInput[1]):
-                    os.chdir(currentPath + "/" + userInput[1])  # implement a try-catch
-                else:
-                    os.write(1, (userInput[1] + " is not a directory.\n").encode())
-            else:
-                os.wait()
-
-
-
-        if userInput[0] == "ls":
-            directories = os.listdir()  #Get directories and files inside directory
-            for directory in directories:
-                if os.path.isdir(currentPath + "/" + directory):
-                    print('\033[94m' + directory, end=" ")
-                else:
-                    print("\033[30m" + directory, end=" ")
-            print("\n")
+        os.write(1, sys.ps1.encode())
+        userInput = input()
+        executeCommand(userInput)
 
 except KeyboardInterrupt:
     os.write(1,("\nProcess Finished with exit code 0").encode())
